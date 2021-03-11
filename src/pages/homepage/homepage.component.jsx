c373746ef31d8613ea693e833cc6c6daab2c6b10
Michel Amiri <misel.amiri@gmail.com>
2021-03-11 12:08:09 +0000

    styling  menu item component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,48 +1,12 @@
 import React from 'react';
+
+import Directory from '../../components/directory/directory.component';
 import './homepage.style.scss';
 
 const HomePage= () => (
-
 <div className ='homepage'>
- <div className ='directory-menu'>
-  
-  <div className='menu-item'>
-    <div className='content'>
-     <h1 className='text'>HATS</h1>
-     <span className='subtitle'>SHOP NOW</span>
-    </div>
-  </div>
-  
-  <div className='menu-item'>
-    <div className='content'>
-     <h1 className='text'>JACKETS</h1>
-     <span className='subtitle'>SHOP NOW</span>
-    </div>  
-  </div>
-  
-  <div className='menu-item'>
-    <div className='content'>
-     <h1 className='text'>SNEAKERS</h1>
-     <span className='subtitle'>SHOP NOW</span>
-    </div>
-  </div>
-  
-  <div className='menu-item'>
-    <div className='content'>
-     <h1 className='text'>WOMENS</h1>
-     <span className='subtitle'>SHOP NOW</span>
-    </div>
-   </div>
-   
-   <div className='menu-item'>
-    <div className='content'>
-     <h1 className='text'>MENS</h1>
-     <span className='subtitle'>SHOP NOW</span>
-    </div>
-   </div>
- 
- </div>
+ <Directory/>
 </div>
-)
+);
 
 export default HomePage;
