4ce159d9cbc4ea88433d67c7c7ba992bae7dad5e
Michel Amiri <misel.amiri@gmail.com>
2021-04-12 16:50:26 +0100

    update our menu item

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import {Switch,Route} from 'react-router-dom';
+import {Route,Link} from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
 
+const HatsPage = () =>(
+  <div>
+   <h1>HATS PAGE</h1>
+  </div>
+	);
+
 function App() {
   return (
     <div>
-      <HomePage/>
+    
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/' component={HatsPage}/>
+    
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,4 +6,8 @@ import './index.css';
 import App from './App';
 
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(
+	<BrowserRouter>
+	<App />
+	</BrowserRouter>,
+	document.getElementById('root'));
