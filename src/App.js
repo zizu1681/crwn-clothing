a2d1790abaa8d540efde73f51c6c8f21b5eee87f
Michel Amiri <misel.amiri@gmail.com>
2021-04-13 12:20:13 +0100

    updating our style

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import {Route,Link} from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
-
-const HatsPage = () =>(
-  <div>
-   <h1>HATS PAGE</h1>
-  </div>
-	);
+import ShopPage from './pages/shop/shop.component.jsx';
 
 function App() {
   return (
     <div>
     
       <Route exact path='/' component={HomePage}/>
-      <Route exact path='/' component={HatsPage}/>
+      <Route path='/shop' component={ShopPage}/>
     
     </div>
   );
